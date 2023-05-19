1e6ee284fc96a3beb6111bdaa56717fe6afef6f6
Andrey Karabeshkin <karabeshkin.a@mail.ru>
2023-05-19 16:08:08 +0300

    CloudDB

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const selectViewRouter = require('./routes/view/select.routes');
 
 //housing
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT || 4000;
 
 serverConfig(app);
 
